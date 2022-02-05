c45adb971a20804eabe4ea4b65a88b8092821ab8
lui / るい <93391439+08rly@users.noreply.github.com>
2022-02-05 17:05:13 +0900

    Update 193.py

--- C問題/193.py
+++ C問題/193.py
@@ -1,3 +1,4 @@
+"""
 #B 
 n = int(input())
 ans = 10**10
@@ -11,3 +12,22 @@ if ans == 10**10:
     print(-1)
 else:
     print(ans)
+"""
+import math
+import itertools
+n, k = map(int, input().split())
+t = [list(map(int, input().split()))for _ in range(n)]
+pat = (math.factorial(n-1))
+per = [i for i in range(1, n, 1)]
+ans = 0
+for x in itertools.permutations(per):
+    now = 0
+    distance = 0
+
+    for y in x:
+        distance += t[now][y]
+        now = y
+    distance += t[now][0]
+    if distance == k:
+        ans += 1
+print(ans)
