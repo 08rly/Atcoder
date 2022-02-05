b89618adc75228baed1a2c84028ee0d595f45f7f
lui / るい <93391439+08rly@users.noreply.github.com>
2022-02-05 16:33:20 +0900

    Delete B問題 directory

--- B問題/193.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-ans = 100000000000000000000
-for i in range(n):
-    a, p, x = map(int, input().split())
-    if x > a:
-        ans = min(ans, p)
-        
-print(ans if ans != 100000000000000000000 else -1)
